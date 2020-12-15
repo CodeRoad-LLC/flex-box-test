aedf6828daf8782fdc7f9ef0c158b19b2b3700f5
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2020-12-14 19:54:30 -0500

    welcomScren

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.part1}>
+     <Image style={styles.img} source={require("./assets/background.jpg")}/>
+      </View>
+      <View style={styles.part2}>
+      </View>
+      <View style={styles.part3}>
+      </View>
+    
     </View>
   );
 }
@@ -17,5 +23,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  img: {
+    width: 200,
+    height: 200,
+  },
+  part1: {
+    flex: 10,
+    width: '100%',
+  },
+  part2: {
+    flex: 1,
+    width:'100%',
+    backgroundColor: '#fc5c65',
+  },
+  part3: {
+    flex: 1,
+    width:'100%',
+    backgroundColor: '#4ECDC4',
   },
 });
